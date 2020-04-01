9a68f568d64fa5709515c951b3b2f076304ddd4e
upkar <uchaudhary57@gmail.com>
2020-04-01 14:07:52 +0530

    ver 1.1

---
+++ jspHibernate/src/main/resources/Bean.java
@@ -0,0 +1,16 @@
+
+public class Bean {
+private String name,password;
+public void setName(String name) {
+	this.name=name;
+}
+public String getName() {
+return name;}
+
+public void setPassword(String password) {
+	this.password=password;
+}
+public String getPassword() {
+return password;}
+}
+
